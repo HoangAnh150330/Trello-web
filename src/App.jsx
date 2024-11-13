c54be6eab90896b9d12e5bf23e6ab2d279c2193a
Hoang Anh <hoanganhtt11@gmail.com>
2024-11-13 23:34:54 +0700

    basic_layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
-import { useColorScheme} from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
+import { useColorScheme } from '@mui/material/styles'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
@@ -14,6 +7,7 @@ import Box from '@mui/material/Box'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Container from '@mui/material/Container'
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
@@ -43,7 +37,7 @@ function ModeSelect() {
           </Box>
         </MenuItem>
         <MenuItem value="system">
-          <Box sx={{display:'flex',alignItems:'center', gap:2}}>
+          <Box sx={{display:'flex',alignItems:'center', gap:1}}>
             <SettingsBrightnessIcon fontSize='small'/>System
           </Box>
         </MenuItem>
@@ -51,43 +45,38 @@ function ModeSelect() {
     </FormControl>
   );
 }
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  const preferDarkMode = useMediaQuery('(prefers-color-scheme :dark)')
-  const preferLightMode = useMediaQuery('(prefers-color-scheme :light)')
-  console.log('prefersDarkMode:', preferDarkMode)
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  );
-}
+
 function App() {
   return (
-    <>
-      <ModeSelect/>
-      <hr />
-      <ModeToggle/>
-      <hr />
-      <div>Trung qaun dev</div>
-      <Typography variant='body2' color='text.secondary'>Test Typography</Typography>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <AccessAlarmIcon/>
-      <ThreeDRotation/>
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height : '100vh', backgroundColor:'primary.main' }}>
+      <Box sx={{
+        backgroundColor:'primary.light',
+        width:'100%',
+        height:(theme) => theme.trello.appBarHeight,
+        display:'flex',
+        alignItems:'center'
+      }}>
+        <ModeSelect/>
+      </Box>
+      <Box sx={{
+        backgroundColor:'primary.dark',
+        width:'100%',
+        height:(theme) => theme.trello.boardBarHeight,
+        display:'flex',
+        alignItems:'center'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor:'primary.main',
+        width:'100%',
+        height:(theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display:'flex',
+        alignItems:'center'
+      }}>
+        Board Content
+      </Box>
+    </Container>
   )
 }
 
