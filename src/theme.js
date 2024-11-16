de27f4e6348e544a3403eac8025d9ea918a19914
Hoang Anh <hoanganhtt11@gmail.com>
2024-11-16 21:15:15 +0700

    App bar p2

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,4 @@
+import { BorderColor } from '@mui/icons-material'
 import { deepOrange, orange, teal, cyan } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 // Create a theme instance.
@@ -10,13 +11,50 @@ const theme = extendTheme({
     light: {
       palette: {
         primary: teal,
-        secondary :deepOrange
+        secondary: deepOrange
       }
     },
     dark: {
       palette: {
         primary: cyan,
         secondary :orange
+      }
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) =>({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        })
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem',
+          '.MuiOutlinedInput-notchedOutline':{
+            borderColor :theme.palette.primary.light
+          },
+          '&:hover':{
+            '.MuiOutlinedInput-notchedOutline':{
+              borderColor :theme.palette.primary.main
+            }
+          },
+          '& fieldset':{
+            borderWidth:'1px !important'
+          }
+
+        })
       }
     }
   }
